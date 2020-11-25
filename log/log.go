a6c97eaacdd25918a4b03e00d60fdbcb92db43ef
Debapriya Das <Debapriya.Das@ril.com>
2020-11-25 16:44:38 +0530

    logrus

--- log/log.go
+++ log/log.go
@@ -1,9 +1,69 @@
 package log
 
 import (
-	log "github.com/sirupsen/logrus"
+	"fmt"
+	"issue-creator/config"
+	"issue-creator/middleware"
+	"os"
+	"path"
+	"time"
+
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
+	"github.com/sirupsen/logrus"
 )
 
-func init() {
-	log.SetReportCaller(true)
+var Logger *logrus.Logger
+
+func LogInit() {
+	logFilePath := config.Log_FILE_PATH
+	logFileName := config.LOG_FILE_NAME
+	fileName := path.Join(logFilePath, logFileName)
+	_, err := middleware.PathExists(logFilePath)
+	if err != nil {
+		os.MkdirAll(logFilePath, os.ModePerm)
+	}
+	src, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+	logger := logrus.New()
+	logger.Out = src
+	logger.SetLevel(logrus.DebugLevel)
+
+	logWriter, err := rotatelogs.New(
+		fileName+".%Y%m%d.log",
+		rotatelogs.WithLinkName(fileName),
+		rotatelogs.WithMaxAge(7*24*time.Hour),
+		rotatelogs.WithRotationTime(24*time.Hour),
+	)
+
+	writeMap := lfshook.WriterMap{
+		logrus.InfoLevel:  logWriter,
+		logrus.FatalLevel: logWriter,
+		logrus.DebugLevel: logWriter,
+		logrus.WarnLevel:  logWriter,
+		logrus.ErrorLevel: logWriter,
+		logrus.PanicLevel: logWriter,
+	}
+
+	lfHook := lfshook.NewHook(writeMap, &logrus.JSONFormatter{
+		TimestampFormat: "20060102T150405,123+0530",
+	})
+
+	// 新增钩子
+	logger.AddHook(lfHook)
+	logger.SetReportCaller(true)
+	Logger = logger
+}
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
--- middleware/mw.go
+++ middleware/mw.go
@@ -1,60 +1,31 @@
 package middleware
 
 import (
-	"fmt"
-	"issue-creator/config"
-	"os"
-	"path"
+	"issue-creator/log"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
-// Log to file
 func LoggerToFile() gin.HandlerFunc {
-	logFilePath := config.Log_FILE_PATH
-	logFileName := config.LOG_FILE_NAME
-	//log file
-	fileName := path.Join(logFilePath, logFileName)
-	//write file
-	src, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
-	if err != nil {
-		fmt.Println("err", err)
-	}
-	//instantiation
-	logger := logrus.New()
-	//Set output
-	logger.Out = src
-	//Set log level
-	logger.SetLevel(logrus.DebugLevel)
-	//Format log
-	logger.SetFormatter(&logrus.TextFormatter{})
+
 	return func(c *gin.Context) {
-		// start time
 		startTime := time.Now()
-		// Processing request
 		c.Next()
-		// End time
 		endTime := time.Now()
-		// execution time
 		latencyTime := endTime.Sub(startTime)
-		// Request mode
 		reqMethod := c.Request.Method
-		// Request routing
 		reqUri := c.Request.RequestURI
-		// Status code
 		statusCode := c.Writer.Status()
-		// Request IP
 		clientIP := c.ClientIP()
-		// Log format
-		logger.Infof("| %3d | %13v | %15s | %s | %s |",
-			statusCode,
-			latencyTime,
-			clientIP,
-			reqMethod,
-			reqUri,
-		)
+		log.Logger.WithFields(logrus.Fields{
+			"status_code":  statusCode,
+			"latency_time": latencyTime,
+			"client_ip":    clientIP,
+			"req_method":   reqMethod,
+			"req_uri":      reqUri,
+		}).Info()
 	}
 }
 
